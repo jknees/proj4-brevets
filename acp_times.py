368d7a0302ba921d5de3d92bd8661541246431fa
jknees <cortezcardinal@gmail.com>
2016-10-24 13:09:42 -0700

    Debugging

--- acp_times.py
+++ acp_times.py
@@ -43,7 +43,7 @@ def open_time( control_dist_km, brevet_dist_km, brevet_start_time ):
 
 
     integer, floating = math.modf(control_start_time)
-    print("hour= " + integer)
+    print("hour= " + str(integer))
     return brevet_start_time.replace(hours=+integer)
 
 def close_time( control_dist_km, brevet_dist_km, brevet_start_time ):
