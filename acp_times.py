94ceb960695a7af55c269c06aaaab9e9f783a309
jknees <cortezcardinal@gmail.com>
2016-10-24 17:37:31 -0700

    Debug

--- acp_times.py
+++ acp_times.py
@@ -35,6 +35,7 @@ def open_time( control_dist_km, brevet_dist_km, brevet_start_time ):
     print("control_dist_km= " + str(control_dist_km))
     control_start_time = 0
     for key in range(len(BREVET_ORDERED_KEYS)):
+      print(key)
       key = BREVET_ORDERED_KEYS[key+1]
       key -= BREVET_ORDERED_KEYS[key]
       if key <= control_dist_km:
