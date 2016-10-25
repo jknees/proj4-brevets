6f5f489fd262a3333ba6bd97f61d4c5e71eea431
jknees <cortezcardinal@gmail.com>
2016-10-24 20:05:26 -0700

    Fixed wrong test check

--- test_acp.py
+++ test_acp.py
@@ -54,9 +54,9 @@ def test_general_times():
 	print()
 
 	print(acp_times.open_time(650.5, 1000, a))
-	assert acp_times.open_time(650.5, 1000, a) == arrow.get('2017-01-02T09:05:00+00:00')
+	assert acp_times.open_time(650.5, 1000, a) == arrow.get('2017-01-01T20:37:00+00:00')
 	print(acp_times.close_time(650.5, 1000, a))
-	assert acp_times.close_time(650.5, 1000, a) == arrow.get('2017-01-04T03:00:00+00:00')
+	assert acp_times.close_time(650.5, 1000, a) == arrow.get('2017-01-02T20:38:00+00:00')
 
 def test_above_and_below_edge_values():
 	answers = [arrow.get('2017-01-01T13:30:00+00:00'), arrow.get('2017-01-01T20:00:00+00:00'), 
