d7bc38411fb570e088d5916ae22a2716efc1b91d
jknees <cortezcardinal@gmail.com>
2016-10-24 17:44:40 -0700

    More list problems

--- acp_times.py
+++ acp_times.py
@@ -39,9 +39,9 @@ def open_time( control_dist_km, brevet_dist_km, brevet_start_time ):
       key -= BREVET_ORDERED_KEYS[i]
       if key <= control_dist_km:
         control_dist_km -= key
-        control_start_time += key/BREVET_TIMES[key][1]
+        control_start_time += key/BREVET_TIMES[i][1]
       elif control_dist_km > 0:
-        control_start_time += control_dist_km/BREVET_TIMES[key][1]
+        control_start_time += control_dist_km/BREVET_TIMES[i][1]
         control_dist_km = 0
 
     print("control_start_time= " + str(control_start_time))
